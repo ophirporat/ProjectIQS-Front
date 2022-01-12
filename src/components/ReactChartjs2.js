17bf6f0130a07643c902af8d83e99f5d22a95aea
ophirporat <57257360+ophirporat@users.noreply.github.com>
2022-01-12 19:57:56 +0200

    chart update

--- src/components/ReactChartjs2.js
+++ src/components/ReactChartjs2.js
@@ -9,71 +9,56 @@ class ReactChartjs2 extends Component {
   constructor(props, ref) {
     super(props)
     // props.setTitle('React Chartjs 2 - Charts')
-    this.showAlert = this.showAlert.bind(this)
+    // this.showAlert = this.showAlert.bind(this)
     console.log("chart created")
-   
-
-  }
-  state={
-      datasets: [{
-        label: 'MMD',
-        data:  [],
-        borderWidth: 1,
-        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-        borderColor: '#E91E63',
-        // borderDash: [],
-      }],
-      labels:this.props.Iteration,
-      sum_wmd:0,
-      iterations:1
   }
 
-  showAlert = (event) => {
-    console.log("event raised ")
-    console.log(this.state.labels)
-    // console.log(this.state.datasets[0].data.length)
-    var new_data = this.state.datasets[0].data
-    var sum_of_dataset=0
-    // for(var i =0;i<new_data.length, new_data[i] != null;i++){
-    //     sum_of_dataset = sum_of_dataset + new_data[i]
-    // }
-    // var wmd_sum = (sum_of_dataset + event["detail"])/this.state.iterations
-    var new_sum_wmd = this.state.sum_wmd + event
-    new_data.push(new_sum_wmd/this.state.iterations)
-    this.setState({"sum_wmd": new_sum_wmd})
-    console.log(new_sum_wmd)
-    this.setState({"iterations": this.state.iterations + 1})
-    // console.log(new_data)
-    
-    
-    // console.log("event detail ", event["detail"])
-    // console.log(event["detail"])
-    // setDatasets( [{
-    //   data:  new_data,
-    //   borderWidth: 1,
-    //   backgroundColor: 'rgba(255, 193, 7, 0.3)',
-    //   borderColor: '#FFC107',
-    //   borderDash: [5, 5],
-    //   fill: false
-    // }])
-    this.setState({"datasets":  [{
-        label: 'MMD',
-        data:  new_data,
-        borderWidth: 1,
-        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-        borderColor: '#E91E63',
-        // borderDash: [5],
-        // fill: false
-      }]})
-      this.setState({"labels": this.props.Iteration})
-    // var new_labels = this.state.labels
-    // new_labels.push(String(this.state.iterations))
-    // var it  = this.state.iterations +1
-    // this.setState({"iterations": it})
-    // setLabels(new_labels)
-    // this.setState({"labels": new_labels})
-    // this.forceUpdate()
+  componentDidMount(){
+    console.log(this.props.dataset)
+    console.log(this.props.labels)
   }
+  // state={
+  //     datasets: [{
+  //       label: 'MMD',
+  //       data:  [],
+  //       borderWidth: 1,
+  //       backgroundColor: 'rgba(233, 30, 99, 0.3)',
+  //       borderColor: '#E91E63',
+  //       fill: true
+  //       // borderDash: [],
+  //     }],
+  //     // labels:this.props.Iteration,
+  //     labels:[1,2,3,4,5,6,7],
+  //     sum_wmd:0,
+  //     iterations:1
+  // }
+
+  // showAlert = (event) => {
+  //   console.log("event raised ")
+  //   console.log(this.state.labels)
+
+  //   // var new_data = this.state.datasets[0].data
+
+
+  //   // var new_sum_wmd = this.state.sum_wmd + event
+  //   // new_data.push(new_sum_wmd/this.state.iterations)
+  //   // this.setState({"sum_wmd": new_sum_wmd})
+  //   // console.log(new_sum_wmd)
+  //   // this.setState({"iterations": this.state.iterations + 1})
+
+  //   this.setState({"datasets":  [{
+  //       label: 'MMD',
+  //       data:  [0.348,0.301, 0.29999, 0.276, 0.269, 0.266, 0.2],
+  //       borderWidth: 1,
+  //       backgroundColor: 'rgba(233, 30, 99, 0.3)',
+  //       borderColor: '#E91E63',
+  //       fill: true
+  //       // borderDash: [5],
+  //       // fill: false
+  //     }]})
+  //     this.setState({"labels": this.props.Iteration})
+
+  // }
   render() {
     const options = {
       responsive: true,
@@ -109,7 +94,7 @@ class ReactChartjs2 extends Component {
           <span className="text-muted font-weight-light">Charts /</span> React Chartjs 2
         </h4> */}
         <h4 className="font-weight-bold py-3 mb-4">
-          <span className="text-muted font-weight-light">Analytics /</span> MMD
+          <span className="text-muted font-weight-light">Analytics /</span> Mean Word’s Mover Distance Graph
         </h4>
 
 
@@ -121,8 +106,17 @@ class ReactChartjs2 extends Component {
               
               height={250}
               data={{
-                labels: this.state.labels,
-                datasets: this.state.datasets
+                labels: this.props.labels,
+                datasets:  [{
+                        label: 'MMD',
+                        data:  this.props.dataset,
+                        borderWidth: 1,
+                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                        borderColor: '#E91E63',
+                        fill: true
+                        // borderDash: [5],
+                        // fill: false
+                      }]
               }}
               options={{
                 ...options,
